ff3add8210fb52453106e1a5ec408a0f49e4b223
Sergio Durán <def.serch@gmail.com>
2019-03-19 19:32:06 -0600

    Agregadas propiedades y relaciones a los modelos

--- app/Product.php
+++ app/Product.php
@@ -2,9 +2,39 @@
 
 namespace App;
 
+use App\Seller;
+use App\Category;
+use App\Transaction;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    const AVAILABLE_PRODUCT = "available";
+    const NOT_AVAILABLE_PRODUCT = "not available";
+
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id',
+    ];
+
+    public function isAvailable() {
+        return $this->status == Product::AVAILABLE_PRODUCT;
+    }
+
+    public function categories() {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function seller() {
+        return $this->belongsTo(Seller:class);
+    }
+
+    public function transactions() {
+        return $this->hasMany(Transaction::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -10,13 +10,24 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    const VERIFIED_USER = '1';
+    const NOT_VERIFIED_USER = '0';
+
+    const ADMIN_USER = 'true';
+    const REGULAR_USER = 'false';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'verified',
+        'verification_token',
+        'admin',
     ];
 
     /**
@@ -25,7 +36,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 
+        'remember_token',
+        'verification_token',
     ];
 
     /**
@@ -36,4 +49,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public isVerified() {
+        return $this->verified == USER::VERIFIED_USER;
+    }
+
+    public isAdmin() {
+        return $this->admin == USER::ADMIN_USER;
+    }
+
+    public static function generateVerificationToken() {
+        return str_random(40);
+    }
 }
