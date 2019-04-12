a24d6e3cb06601814bafe9331f52020d29776dff
Serch Durán <def.serch@gmail.com>
2019-04-12 02:40:38 -0500

    Se agrego Laravel Passport al proyecto permitiendo así usar Oauth2 como modo de authenticacion

---
+++ resources/views/home/authorized-clients.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <passport-authorized-clients></passport-authorized-clients>
+    </div>
+</div>
+
+@endsection
